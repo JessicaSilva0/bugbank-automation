288eba58da5d3b3ded9de1ba07c6fff2ec42b2e7
Jess <jessica.silva@soluttions.com.br>
2025-08-02 00:33:13 +0100

    update login.spec.ts
    + add playwright-tag-linter

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -4,13 +4,31 @@ const email = process.env.EMAIL;
 const senha = process.env.SENHA;
 
 test.describe("Bug Bank - Página Inicial", async () => {
-  test("BB-CT-001 - Acessar Conta com visualização de Senha", async ({ loginPage }) => {
-    await loginPage.acessarConta(email!, senha!, true);
-  });
-  test("BB-CT-002 - Acessar Conta sem visualização de Senha", async ({ loginPage }) => {
-    await loginPage.acessarConta(email!, senha!);
-  });
-  test("BB-CT-003 - Acessar Conta sem Sucesso", async ({ loginPage }) => {
-    await loginPage.acessarConta("", "");
-  });
+  test(
+    "BB-CT-001 - Acessar Conta com visualização de Senha",
+    {
+      tag: ["@critical"],
+    },
+    async ({ loginPage }) => {
+      await loginPage.acessarConta(email!, senha!, true);
+    },
+  );
+  test(
+    "BB-CT-002 - Acessar Conta sem visualização de Senha",
+    {
+      tag: ["@high"],
+    },
+    async ({ loginPage }) => {
+      await loginPage.acessarConta(email!, senha!);
+    },
+  );
+  test(
+    "BB-CT-003 - Acessar Conta sem Sucesso",
+    {
+      tag: ["@medium"],
+    },
+    async ({ loginPage }) => {
+      await loginPage.acessarConta("", "");
+    },
+  );
 });
